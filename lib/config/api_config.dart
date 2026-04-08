f5e5d6970fe54abbb80835eb8d5d06356978822f
ecoruta05 <ecoruta05@gmail.com>
2026-04-08 13:44:10 -0500

    RAILWAY - IA - APK

--- lib/config/api_config.dart
+++ lib/config/api_config.dart
@@ -1,24 +1,14 @@
 class ApiConfig {
   ApiConfig._();
 
-  static const String _defaultHost = String.fromEnvironment(
-    'ECORUTA_API_HOST',
-    defaultValue: '192.168.44.88',
+  static const String authBaseUrl = String.fromEnvironment(
+    'ECORUTA_AUTH_URL',
+    defaultValue: 'https://ecoruta05-production.up.railway.app',
   );
 
-  static const String _authPort = String.fromEnvironment(
-    'ECORUTA_AUTH_PORT',
-    defaultValue: '3000',
+  static const String iaBaseUrl = String.fromEnvironment(
+    'ECORUTA_IA_URL',
+    defaultValue:
+        'https://captivating-perfection-production-3c07.up.railway.app',
   );
-
-  static const String _iaPort = String.fromEnvironment(
-    'ECORUTA_IA_PORT',
-    defaultValue: '8000',
-  );
-
-  static String get host => _defaultHost;
-
-  static String get authBaseUrl => 'http://$host:$_authPort';
-
-  static String get iaBaseUrl => 'http://$host:$_iaPort';
 }
